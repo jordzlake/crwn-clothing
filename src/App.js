bbe009c01001ac96480471e29aa0428f0381812c
Jordan Lake <jordanlake18@outlook.com>
2022-10-29 13:16:39 -0400

    updated menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 import React from "react";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = (props) => (
+  <div>
+    <button onClick={() => props.history.push("/")}>Home</button>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
