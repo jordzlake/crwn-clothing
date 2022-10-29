68904a0236a8d9597ea57acc3bcdc2d6cd89f79f
Jordan Lake <jordanlake18@outlook.com>
2022-10-29 15:34:20 -0400

    added header and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import React from "react";
 import ShopPage from "./pages/shoppage/shop.component";
-
 import HomePage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
