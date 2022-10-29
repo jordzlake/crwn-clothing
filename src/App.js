608b452dd210942487f8e3060849d050e458842a
Jordan Lake <jordanlake18@outlook.com>
2022-10-29 14:24:16 -0400

    updating app with shoppage collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import React from "react";
+import ShopPage from "./pages/shoppage/shop.component";
 
 import HomePage from "./pages/homepage/homepage.component";
 
-const HatsPage = (props) => (
-  <div>
-    <button onClick={() => props.history.push("/")}>Home</button>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
 function App() {
   return (
-    <div>
+    <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
-    </div>
+      <Route path="/shop" component={ShopPage} />
+    </Switch>
   );
 }
 
